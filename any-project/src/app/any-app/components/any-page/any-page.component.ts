6ea9679f249dcc979fb913d7341489391e91048f
nata-water <nata-water@users.noreply.github.com>
2021-09-13 22:45:52 +0900

    #first_django_branch 正常パターン動作確認

--- any-project/src/app/any-app/components/any-page/any-page.component.ts
+++ any-project/src/app/any-app/components/any-page/any-page.component.ts
@@ -10,6 +10,7 @@ export class AnyPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log("Hello Angular")
   }
 
 }
--- some_project/some_api/admin.py
+++ some_project/some_api/admin.py
@@ -1,3 +1,4 @@
-# from django.contrib import admin
-
-# Register your models here.
+from django.contrib import admin
+from some_project.some_api.models import Manufacturer
+# python-app.ymlの動作確認用
+admin.site.register(Manufacturer)
