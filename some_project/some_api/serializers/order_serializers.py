090141c80fc7febd8b4e1bb643a74a249e95d434
Shinnosuke Haba <>
2021-09-16 00:29:28 +0900

    #order_api 一旦コミット

--- some_project/some_api/serializers/order_serializers.py
+++ some_project/some_api/serializers/order_serializers.py
@@ -1,7 +1,40 @@
 # coding: utf-8
 from rest_framework import serializers
-from ..models import Order
+from ..models import Order, OrderDetail
 from db.models import BaseModel
+
+
+class OrderDetailSerializer(serializers.ModelSerializer):
+    """OrderDetailのSerializer"""
+
+    product_name = serializers.CharField(source="product.name", read_only=True)
+
+    class Meta:
+        model = OrderDetail
+        fields = [
+            "id",
+            "product",
+            "product_name",
+            "num",
+        ] + [base_field.name for base_field in BaseModel._meta.get_fields()]
+
+
+class NestedOrderSerializer(serializers.ModelSerializer):
+    """Order, OrderDetail のSerializer"""
+
+    order_details = OrderDetailSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = [
+            "id",
+            "order_person",
+            "order_day",
+            "description",
+            "is_express",
+            "status",
+            "order_details",
+        ] + [base_field.name for base_field in BaseModel._meta.get_fields()]
 
 
 class OrderSerializer(serializers.ModelSerializer):
