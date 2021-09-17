d92adc1eaf766a002c97facde354dedc0e694ea5
nata-water <nata-water@users.noreply.github.com>
2021-09-17 21:45:36 +0900

    [update] バックエンドの初期セットアップ

--- some_project/some_api/admin.py
+++ some_project/some_api/admin.py
@@ -1,1 +1,22 @@
 from django.contrib import admin
+
+from some_api.models import Order, OrderDetail
+
+
+class OrderDetailInline(admin.TabularInline):
+    model = OrderDetail
+
+
+@admin.register(Order)
+class OrderHeaderAdmin(admin.ModelAdmin):
+    fields = ("order_person", "order_day", "description", "is_express", "status")
+    list_display = (
+        "order_person",
+        "order_day",
+        "description",
+        "is_express",
+        "status",
+        "cre_dt",
+        "upd_dt",
+    )
+    inlines = [OrderDetailInline]
