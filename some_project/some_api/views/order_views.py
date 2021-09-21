7a8e06b7fed2a4c0b477308b24b0ffc2b21c4b2d
Shinnosuke Haba <>
2021-09-21 22:51:29 +0900

    [update] 管理項目を設定するメソッドのリファクタリング

--- some_project/some_api/views/order_views.py
+++ some_project/some_api/views/order_views.py
@@ -10,7 +10,11 @@ from ..serializers.order_serializers import (
     CreateNestedOrderSerializer,
 )
 from db.exceptions import TargetRecordDoesNotExist
-from utils.helpers import add_fields_to_data, add_fields_to_create_data
+from utils.helpers import (
+    add_fields_to_data,
+    add_fields_to_create_data,
+    resolve_view_name,
+)
 
 
 class OrderViewSet(viewsets.ModelViewSet):
@@ -21,11 +25,12 @@ class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
-    def _create_details(self, request, order, order_datails):
+    def _create_details(self, order, order_datails):
         """注文配下の注文詳細を全登録する"""
+
         for s_od in order_datails:
             od = OrderDetail(order=order, product=s_od["product"], num=s_od["num"])
-            od.fill_common_info(request)
+            od.fill_base_fields(order.upd_user_id, order.upd_pgm_id)
             od.save()
 
     @action(detail=False, methods=["post"])
@@ -41,9 +46,9 @@ class OrderViewSet(viewsets.ModelViewSet):
             is_express=serializer.validated_data["is_express"],
             status=serializer.validated_data["status"],
         )
-        order.fill_common_info(request)
+        order.fill_base_fields(request.user.username, resolve_view_name(request))
         order.save()
-        self._create_details(request, order, serializer.validated_data["order_details"])
+        self._create_details(order, serializer.validated_data["order_details"])
         headers = self.get_success_headers(serializer.data)
         added_data = add_fields_to_create_data(order, serializer.data)
 
@@ -79,14 +84,15 @@ class OrderViewSet(viewsets.ModelViewSet):
             order.status = serializer.validated_data["status"]
             order.upd_dt = serializer.validated_data["upd_dt"]
             try:
+                order.fill_base_fields(
+                    request.user.username, resolve_view_name(request), update=True
+                )
                 order.save_exclusive()
             except TargetRecordDoesNotExist:
                 return Response(status=status.HTTP_423_LOCKED)
 
             OrderDetail.objects.filter(order__id=order.id).delete()
-            self._create_details(
-                request, order, serializer.validated_data["order_details"]
-            )
+            self._create_details(order, serializer.validated_data["order_details"])
             added_data = add_fields_to_data(order, serializer.data)
             headers = self.get_success_headers(serializer.data)
             return Response(added_data, status=status.HTTP_201_CREATED, headers=headers)
--- some_project/utils/helpers.py
+++ some_project/utils/helpers.py
@@ -3,7 +3,11 @@ from collections import OrderedDict
 
 
 def add_fields_to_data(obj: object, data: OrderedDict) -> OrderedDict:
-    """dataを管理項目を付与する"""
+    """dataに管理項目を付与する.主にAPIの戻り値として返すデータを作成するために利用.
+    :param object obj: 管理項目を設定したオブジェクト
+    :param object obj: リクエスト
+    :return: viewの名前(100文字を超える場合は後ろの100文字を返す)
+    """
     sd_list = list(data.items())
     sd_list.insert(len(sd_list) - 1, ("cre_user_id", obj.cre_user_id))
     sd_list.insert(len(sd_list) - 1, ("cre_pgm_id", obj.cre_pgm_id))
@@ -16,9 +20,29 @@ def add_fields_to_data(obj: object, data: OrderedDict) -> OrderedDict:
 
 
 def add_fields_to_create_data(obj: object, data: OrderedDict) -> OrderedDict:
-    """dataに id と管理項目を付与する(追加API用)"""
+    """dataに id と管理項目を付与する.主にデータ追加APIの戻り値として返すデータを作成するために利用."""
     sd_list = list(data.items())
     sd_list.insert(0, ("id", obj.id))
     added_data = OrderedDict(sd_list)
     added_data = add_fields_to_data(obj, added_data)
     return added_data
+
+
+def resolve_view_name(request):
+    """リクエストオブジェクトからviewの名前を返す関数
+    :param request: リクエスト
+    :return: viewの名前(100文字を超える場合は後ろの100文字を返す)
+    """
+    VIEW_CLASS_ATTRIBUTE_NAME = "view_class_name"
+
+    # ViewSetの場合は属性を参照する
+    if hasattr(request, VIEW_CLASS_ATTRIBUTE_NAME):
+        return getattr(request, VIEW_CLASS_ATTRIBUTE_NAME)
+
+    func = request.resolver_match.func
+    if hasattr(func, "__self__"):
+        view_real = func.__self__.__class__
+    else:
+        view_real = func
+
+    return "{}.{}".format(view_real.__module__, view_real.__name__)[-100:]
