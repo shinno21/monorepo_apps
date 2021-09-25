5fe011eaee85963ecd9aa02d8eaa781d21dbc4de
nata-water <nata-water@users.noreply.github.com>
2021-09-25 19:17:43 +0900

    [add] #13_ci_code_formatter

---
+++ any-project/src/app/any-app/components/any-page/any-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-any-page',
+  templateUrl: './any-page.component.html',
+  styleUrls: ['./any-page.component.scss']
+})
+export class AnyPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('Hello Angular');
+  }
+}
