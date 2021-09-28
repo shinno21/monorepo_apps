53c56e469af93eb500388409781388308f9ee43b
Shinnosuke Haba <>
2021-09-28 23:06:29 +0900

    [update] OrderのViewsetを廃止、CRUD個別のクラスビューに変更

--- some_project/some_api/urls.py
+++ some_project/some_api/urls.py
@@ -1,16 +1,26 @@
 from django.conf.urls import url
-from django.urls import include
+from django.urls import path, include
 from rest_framework import routers
 from .views.manufacturer_views import ManufacturerViewSet
 from .views.product_views import ProductViewSet
-from .views.order_views import OrderViewSet
+from .views.order_views import (
+    CreateOrderView,
+    UpdateOrderView,
+    RetrieveOrderView,
+    ListOrderView,
+    DestroyOrderView,
+)
 
 app_name = "some_api"
 
 router = routers.DefaultRouter()
 router.register(r"manufacturers", ManufacturerViewSet)
 router.register(r"products", ProductViewSet)
-router.register(r"orders", OrderViewSet)
 urlpatterns = [
-    url(r"^", include(router.urls)),
+    path(r"^", include(router.urls)),
+    path(r"order/list/", ListOrderView.as_view()),
+    path(r"order/create/", CreateOrderView.as_view()),
+    path(r"order/<str:pk>/", RetrieveOrderView.as_view()),
+    path(r"order/<str:pk>/update/", UpdateOrderView.as_view()),
+    path(r"order/<str:pk>/destroy/", DestroyOrderView.as_view()),
 ]
--- some_project/some_api/views/order_views.py
+++ some_project/some_api/views/order_views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
+from rest_framework.generics import (
+    CreateAPIView,
+    UpdateAPIView,
+    RetrieveAPIView,
+    ListAPIView,
+    DestroyAPIView,
+)
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from ..models import Order, OrderDetail
@@ -16,13 +23,17 @@ from utils.helpers import (
 )
 
 
-class OrderViewSet(viewsets.ModelViewSet):
-    """OrderのViewSet
-    一番シンプルなViewset(ModelViewsetのデフォルト機能のみ)
-    """
+class ListOrderView(ListAPIView):
+    """1件のOrderをOrderDetailを含めて取得する"""
 
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+
+class CreateOrderView(CreateAPIView):
+    """OrderとOrderDetailを登録する"""
+
+    serializer_class = CreateNestedOrderSerializer
 
     def _create_details(self, order, order_datails):
         """注文配下の注文詳細を全登録する"""
@@ -32,11 +43,8 @@ class OrderViewSet(viewsets.ModelViewSet):
             od.fill_base_fields(order.upd_user_id, order.upd_pgm_id)
             od.save()
 
-    @action(detail=False, methods=["post"])
-    def create_nested(self, request):
-        """注文と注文詳細を登録"""
-
-        serializer = CreateNestedOrderSerializer(data=request.data)
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         order = Order(
             order_person=serializer.validated_data["order_person"],
@@ -57,41 +65,59 @@ class OrderViewSet(viewsets.ModelViewSet):
             headers=headers,
         )
 
-    @action(detail=True, methods=["get", "put"])
-    def nested(self, request, pk):
-        """注文と注文詳細を参照/更新"""
 
-        if request.method == "GET":
-            order = get_object_or_404(
-                Order.objects.prefetch_related("order_details")
-                .prefetch_related("order_details__product")
-                .prefetch_related("order_details__product__manufacturer"),
-                pk=pk,
+class UpdateOrderView(UpdateAPIView):
+    """Orderを更新して、OrderDetailを登録する"""
+
+    queryset = Order.objects.all()
+    serializer_class = CreateNestedOrderSerializer
+
+    def _create_details(self, order, order_datails):
+        """注文配下の注文詳細を全登録する"""
+
+        for s_od in order_datails:
+            od = OrderDetail(order=order, product=s_od["product"], num=s_od["num"])
+            od.fill_base_fields(order.upd_user_id, order.upd_pgm_id)
+            od.save()
+
+    def put(self, request, *args, **kwargs):
+        order = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        order.order_person = serializer.validated_data["order_person"]
+        order.order_day = serializer.validated_data["order_day"]
+        order.description = serializer.validated_data["description"]
+        order.is_express = serializer.validated_data["is_express"]
+        order.status = serializer.validated_data["status"]
+        order.upd_dt = serializer.validated_data["upd_dt"]
+        try:
+            order.fill_base_fields(
+                request.user.username, resolve_view_name(request), update=True
             )
-            # instance = self.get_object()
-            serializer = RetrieveNestedOrderSerializer(order)
-            return Response(serializer.data)
+            order.save_exclusive()
+        except TargetRecordDoesNotExist:
+            return Response(status=status.HTTP_423_LOCKED)
 
-        if request.method == "PUT":
-            order = get_object_or_404(Order, pk=pk)
-            serializer = CreateNestedOrderSerializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            order.order_person = serializer.validated_data["order_person"]
-            order.order_day = serializer.validated_data["order_day"]
-            order.description = serializer.validated_data["description"]
-            order.is_express = serializer.validated_data["is_express"]
-            order.status = serializer.validated_data["status"]
-            order.upd_dt = serializer.validated_data["upd_dt"]
-            try:
-                order.fill_base_fields(
-                    request.user.username, resolve_view_name(request), update=True
-                )
-                order.save_exclusive()
-            except TargetRecordDoesNotExist:
-                return Response(status=status.HTTP_423_LOCKED)
+        OrderDetail.objects.filter(order__id=order.id).delete()
+        self._create_details(order, serializer.validated_data["order_details"])
+        added_data = add_fields_to_data(order, serializer.data)
+        headers = self.get_success_headers(serializer.data)
+        return Response(added_data, status=status.HTTP_201_CREATED, headers=headers)
 
-            OrderDetail.objects.filter(order__id=order.id).delete()
-            self._create_details(order, serializer.validated_data["order_details"])
-            added_data = add_fields_to_data(order, serializer.data)
-            headers = self.get_success_headers(serializer.data)
-            return Response(added_data, status=status.HTTP_201_CREATED, headers=headers)
+
+class RetrieveOrderView(RetrieveAPIView):
+    """1件のOrderをOrderDetailを含めて取得する"""
+
+    queryset = (
+        Order.objects.prefetch_related("order_details")
+        .prefetch_related("order_details__product")
+        .prefetch_related("order_details__product__manufacturer")
+    )
+    serializer_class = RetrieveNestedOrderSerializer
+
+
+class DestroyOrderView(DestroyAPIView):
+    """1件のOrderを削除する"""
+
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
